60ea12931b6e2ed23ac00ea7164074b5e1c1425d
Andy Tran <rooneytai5236@gmail.com>
2025-02-06 22:03:01 +1100

    End of section 18

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'api/',
+    hubUrl: 'hub/notifications',
     stripePublicKey: 'pk_test_51Ql9kMIABOP1cLzswwWnl91Hw2E4LMU1s0OzFZktCFD6T0IlsjlqJ21SFGzaXhYHSQ372ZpmNzOAvlPPEmbW14Iy00LOoG3SMv'
 };
